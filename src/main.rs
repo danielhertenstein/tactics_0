90a088a4ec9c7b5df805590a5e418db4e2d72373
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2019-02-21 16:29:35 -0500

    Starts on initial rendering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate tcod;
+
+use tcod::console::*;
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 80;
+
 struct Actor {
     x: i32,
     y: i32,
@@ -5,13 +12,32 @@ struct Actor {
 
 struct GameState {
     actors: Vec<Actor>,
+
+}
+
+struct Renderer {
+    root: Root,
 }
 
 fn main() {
+    let mut renderer = initialize_rendering_engine();
     let mut game_state = initial_game_state();
 
     loop {
-        render_system(&mut game_state);
+        render_system(&renderer, &game_state);
+    }
+}
+
+fn initialize_rendering_engine() -> Renderer {
+    let root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod tutorial")
+        .init();
+    tcod::system::set_fps(LIMIT_FPS);
+    Renderer {
+        root
     }
 }
 
@@ -22,4 +48,8 @@ fn initial_game_state() -> GameState {
             Actor { x: 0, y: 1 },
         ]
     }
+}
+
+fn render_system(renderer: &Renderer, game_state: &GameState) {
+
 }
