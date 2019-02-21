749437834391994bbba5796c99e02c3f517eb133
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2019-02-21 16:16:30 -0500

    Starts on putting things on a screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+struct Actor {
+    x: i32,
+    y: i32,
+}
+
+struct GameState {
+    actors: Vec<Actor>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game_state = initial_game_state();
+
+    loop {
+        render_system(&mut game_state);
+    }
+}
+
+fn initial_game_state() -> GameState {
+    GameState {
+        actors: vec![
+            Actor { x: 0, y: 0 },
+            Actor { x: 0, y: 1 },
+        ]
+    }
 }
